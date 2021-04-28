ec27d2a56c8bc222f22f15aa979fa9ef08d66ac3
Daniel Sorichetti <me@dancodes.com.ar>
2021-04-28 15:57:39 +0200

    rename to microsoft levels, add close menu to tray, run thread in background

--- Windows Volume/MainWindow.xaml.cs
+++ Windows Volume/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
+using System.Windows.Threading;
 
 namespace Windows_Volume
 {
@@ -27,31 +28,100 @@ namespace Windows_Volume
     public partial class MainWindow : Window
     {
         private NotifyIcon ni;
+        private CoreAudioDevice defaultPlaybackDevice;
+        private IDisposable volumeChangedSub;
+        private Thread workerThread;
         public MainWindow()
         {
             InitializeComponent();
 
-            // Set Tray icon
-            ni = new System.Windows.Forms.NotifyIcon();
+            this.WindowState = WindowState.Minimized;
+            Hide();
+
+            SetTrayIcon();
+            setMaxVolume();
+
+            workerThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    setMaxVolume();
+
+                    Thread.Sleep(1000 * 30);
+                }
+            });
+            workerThread.IsBackground = true;
+            workerThread.Start();
+
+            volumeChangedSub = defaultPlaybackDevice.VolumeChanged.Subscribe(new VolumeReporter(this));
+
+            Microsoft.Win32.SystemEvents.PowerModeChanged += OnPowerChange;
+        }
+
+        private void SetTrayIcon()
+        {
+            ni = new NotifyIcon();
             // Path iconPath = 
-            ni.Icon = Windows_Volume.Properties.Resources.pngegg_CL4_icon;
+            ni.Icon = Properties.Resources.pngegg_CL4_icon;
             ni.Visible = true;
+            ni.ContextMenuStrip = CreateContextMenuStrip();
             ni.DoubleClick +=
                 delegate (object sender, EventArgs args)
                 {
                     this.Show();
-                    this.WindowState = System.Windows.WindowState.Normal;
+                    this.WindowState = WindowState.Normal;
                 };
-            
-            ni.ShowBalloonTip(3000, 
-                "Windows Microphone Volume is now running", 
-                "Microphone volume will be permanently set to 100%", 
-                ToolTipIcon.Info);
 
-            CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultCaptureDevice;
+            ni.ShowBalloonTip(1000,
+                "Microphone Levels is now running",
+                "Microphone level will be permanently set to 100%",
+                ToolTipIcon.Info);
+        }
+
+        private ContextMenuStrip CreateContextMenuStrip()
+        {
+            var contextMenuStrip = new ContextMenuStrip();
+            var closeMenuItem = new ToolStripMenuItem("Close");
+            closeMenuItem.Click += CloseMenuItem_Click;
+            contextMenuStrip.Items.Add(closeMenuItem);
+
+            return contextMenuStrip;
+        }
+
+        private void CloseMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        private void OnPowerChange(object s, Microsoft.Win32.PowerModeChangedEventArgs e)
+        {
+            switch (e.Mode)
+            {
+                case Microsoft.Win32.PowerModes.Resume:
+                    volumeChangedSub = defaultPlaybackDevice.VolumeChanged.Subscribe(new VolumeReporter(this));
+                    break;
+                case Microsoft.Win32.PowerModes.Suspend:
+                    volumeChangedSub.Dispose();
+                    break;
+            }
+        }
+
+        public void setMaxVolume()
+        {
+
+            if (defaultPlaybackDevice == null)
+            {
+                defaultPlaybackDevice = new CoreAudioController().DefaultCaptureDevice;
+            }
+
             defaultPlaybackDevice.Volume = 100;
 
-            defaultPlaybackDevice.VolumeChanged.Subscribe(new VolumeReporter());
+            Action action = delegate ()
+            {
+                mainText.Text = String.Format("Level is set to 100%");
+            };
+
+            Dispatcher.Invoke(DispatcherPriority.Normal, action);
         }
 
         // Minimize to system tray when application is minimized.
@@ -61,9 +131,9 @@ namespace Windows_Volume
             {
                 this.Hide();
 
-                ni.ShowBalloonTip(2000,
-                    "Windows Microphone Volume is still running",
-                    "Microphone volume will be permanently set to 100%",
+                ni.ShowBalloonTip(1000,
+                    "Microphone Levels is still running",
+                    "Microphone level will be permanently set to 100%",
                     ToolTipIcon.Info);
             }
 
@@ -75,6 +145,12 @@ namespace Windows_Volume
     {
         private IDisposable unsubscriber;
         private bool updating = false;
+        private MainWindow w;
+
+        public VolumeReporter(MainWindow w)
+        {
+            this.w = w;
+        }
 
 
         public virtual void Subscribe(IObservable<DeviceVolumeChangedArgs> provider)
@@ -89,7 +165,6 @@ namespace Windows_Volume
 
         public virtual void OnCompleted()
         {
-            Console.WriteLine("Additional temperature data will not be transmitted.");
         }
 
         public virtual void OnError(Exception error)
@@ -104,7 +179,7 @@ namespace Windows_Volume
                 updating = true;
 
                 Thread.Sleep(200);
-                value.Device.Volume = 100;
+                w.setMaxVolume();
                 updating = false;
             }
         }
